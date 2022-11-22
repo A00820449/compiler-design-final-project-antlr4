177aee3ecc4f5c0f2a451bc1486621cdff577b17
Miguel Tornero <a00820449@itesm.mx>
2022-11-22 17:59:35 -0600

    added object file generation

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ import GrammarLexer from "./lib/GrammarLexer.js"
 import GrammarParser from "./lib/GrammarParser.js"
 import MyListener from "./my_listener.js"
 import {default as antlr} from "antlr4"
-import {readFileSync} from "node:fs"
+import {readFileSync, writeFileSync} from "node:fs"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
 
-const input = readFileSync("input.txt").toString()
+if (!process.argv[2]) {
+    throw new Error("Usage: node . <input file>")
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const input = readFileSync(process.argv[2]).toString()
 
 const chars = new antlr.InputStream(input)
 const lexer = new GrammarLexer(chars)
@@ -19,4 +29,7 @@ parser.addParseListener(listener)
 console.log("Input:", input)
 parser.start()
 
-console.log({quadruples: listener.quad_vector, constants: listener.consts})
+const output = {quadruples: listener.quad_vector, constants: listener.consts}
+
+writeFileSync(path.resolve(__dirname, "output.o"), JSON.stringify(output, null, 4))
+console.log(output)
