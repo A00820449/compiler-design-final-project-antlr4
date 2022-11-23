b899d3d6e08f4597146f726467faeab3143df0a8
Miguel Tornero <a00820449@itesm.mx>
2022-11-22 20:40:22 -0600

    added vm

---
+++ compiler.js
@@ -0,0 +1,35 @@
+import GrammarLexer from "./lib/GrammarLexer.js"
+import GrammarParser from "./lib/GrammarParser.js"
+import MyListener from "./my_listener.js"
+import {default as antlr} from "antlr4"
+import {readFileSync, writeFileSync} from "node:fs"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
+if (!process.argv[2]) {
+    throw new Error("Usage: node . <input file>")
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const input = readFileSync(process.argv[2]).toString()
+
+const chars = new antlr.InputStream(input)
+const lexer = new GrammarLexer(chars)
+const tokens = new antlr.CommonTokenStream(lexer)
+const parser = new GrammarParser(tokens)
+
+const listener = new MyListener()
+
+parser.buildParseTrees = true
+parser.addParseListener(listener)
+
+//console.log("Input:", input)
+parser.start()
+
+const output = {quadruples: listener.quad_vector, constants: listener.consts}
+
+writeFileSync(path.resolve(__dirname, "output.o"), JSON.stringify(output, null, 4))
+//console.log(output)
--- vm.js
+++ vm.js
@@ -1,27 +1,80 @@
 import fs from "node:fs"
 
-if (process.argv[2]) {
+if (!process.argv[2]) {
     throw new Error("Usage: node vm.js <object file>")
 }
+
+
 
 /**
  * @type {{quadruples: string[][], constants: Object<string, number | boolean>}}
  */
-const {quadruples, constants} = JSON.stringify(fs.readFileSync(process.argv[2]).toString())
+const {quadruples, constants} = JSON.parse(fs.readFileSync(process.argv[2]).toString())
+
 
 const mem = Object.assign({}, constants)
-function getMem(key) {
-    return mem[key] || false
-}
+
+//console.debug(quadruples, constants)
 
 for (let cursor = 0; cursor < quadruples.length; cursor++) {
-    switch (quadruples[i][0]) {
+    const q = quadruples[cursor]
+    switch (q[0]) {
         case "=":
-            
+            mem[q[1]] = mem[q[3]]
             break;
-    
+        case "+":
+            mem[q[3]] = mem[q[1]] + mem[q[2]]
+            break;
+        case "-":
+            mem[q[3]] = mem[q[1]] - mem[q[2]]
+            break;
+        case "*":
+            mem[q[3]] = mem[q[1]] * mem[q[2]]
+            break;
+        case "/":
+            mem[q[3]] = mem[q[1]] / mem[q[2]]
+            break;
+        case "^":
+            mem[q[3]] = Math.pow(mem[q[1]], mem[q[2]])
+            break;
+        case "==":
+            mem[q[3]] = mem[q[1]] === mem[q[2]]
+            break;
+        case "!=":
+            mem[q[3]] = mem[q[1]] !== mem[q[2]]
+            break;
+        case "|":
+            mem[q[3]] = mem[q[1]] || mem[q[2]]
+            break;
+        case "&":
+            mem[q[3]] = mem[q[1]] && mem[q[2]]
+            break;
+        case ">":
+            mem[q[3]] = mem[q[1]] > mem[q[2]]
+            break;
+        case ">=":
+            mem[q[3]] = mem[q[1]] >= mem[q[2]]
+            break;
+        case "<":
+            mem[q[3]] = mem[q[1]] < mem[q[2]]
+            break;
+        case "<=":
+            mem[q[3]] = mem[q[1]] <= mem[q[2]]
+            break;
+        case "print":
+            console.log(mem[q[1]])
+            break;
+        case "a":
+            mem[q[3]] = mem[q[1]] + mem[q[2]]
+            break;
+        case "b":
+            mem[q[3]] = mem[q[1]] + mem[q[2]]
+            break;
+        case "c":
+            mem[q[3]] = mem[q[1]] + mem[q[2]]
+            break;
         default:
-            throw new Error(`Unrecognized instruction: ${quadruples[i][0]}`)
+            throw new Error(`Unrecognized instruction: ${q[0]}`)
             break;
     }
 }
